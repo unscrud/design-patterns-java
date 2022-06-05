9c3bcf08896113482ee0717f5a66c71ff64c372f
Daniel Carvalho <daniel.devop@gmail.com>
2022-06-05 09:17:20 -0300

    Calculadora de imposto

---
+++ src/dev/unscrud/imposto/CalculadoraDeImposto.java
@@ -0,0 +1,18 @@
+package dev.unscrud.imposto;
+
+import java.math.BigDecimal;
+
+import dev.unscrud.orcamento.Orcamento;
+
+public class CalculadoraDeImposto {
+  public BigDecimal calcular(Orcamento orcamento, TipoImposto tipoImposto) {
+    switch (tipoImposto) {
+      case ICMS:
+        return orcamento.getValor().multiply(new BigDecimal("0.1"));
+      case ISS:
+        return orcamento.getValor().multiply(new BigDecimal("0.06"));
+      default:
+        return BigDecimal.ZERO;
+    }
+  }
+}
