de8679cdfb093b264a26123ae209ec6c8573b8ea
Daniel Carvalho <daniel.devop@gmail.com>
2022-06-13 21:33:09 -0300

    Alteração do teste

--- src/dev/unscrud/teste/TestesDescontos.java
+++ src/dev/unscrud/teste/TestesDescontos.java
@@ -7,7 +7,7 @@ import dev.unscrud.orcamento.Orcamento;
 
 public class TestesDescontos {
   public static void main(String[] args) {
-    Orcamento primeiro = new Orcamento(new BigDecimal("200"), 4);
+    Orcamento primeiro = new Orcamento(new BigDecimal("200"), 6);
     Orcamento segundo = new Orcamento(new BigDecimal("1000"), 1);
     CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
     System.out.println(calculadora.calcular(primeiro));
