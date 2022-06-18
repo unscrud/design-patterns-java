9c556f8cb0559207fd2982afe702896a6076ecce
Daniel Carvalho <daniel.devop@gmail.com>
2022-06-18 12:16:25 -0300

    Aplicando o Template Method

--- src/dev/unscrud/desconto/CalculadoraDeDescontos.java
+++ src/dev/unscrud/desconto/CalculadoraDeDescontos.java
@@ -6,11 +6,11 @@ import dev.unscrud.orcamento.Orcamento;
 
 public class CalculadoraDeDescontos {
   public BigDecimal calcular(Orcamento orcamento) {
-    Desconto desconto;
-    desconto = new DescontoParaOrcamentoComMaisDeCincoItens(
+    Desconto cadeiaDeDescontos;
+    cadeiaDeDescontos = new DescontoParaOrcamentoComMaisDeCincoItens(
         new DescontoParaOrcamentoComValorMaiorQueQuinhentos(
             new SemDesconto()));
 
-    return desconto.calcular(orcamento);
+    return cadeiaDeDescontos.efetuarCalculo(orcamento);
   }
 }
