24789f41fdf4555240f607489f6e072eef3c3b7d
Daniel Carvalho <daniel.devop@gmail.com>
2022-06-25 12:12:50 -0300

    Classe responsável pelo registro de orcamento

---
+++ src/dev/unscrud/orcamento/RegistroDeOrcamento.java
@@ -0,0 +1,10 @@
+package dev.unscrud.orcamento;
+
+public class RegistroDeOrcamento {
+  public void registrar(Orcamento orcamento) {
+    // Chamada HTTP para a API externa
+    // url connection
+    // http client
+    // lib rest
+  }
+}
